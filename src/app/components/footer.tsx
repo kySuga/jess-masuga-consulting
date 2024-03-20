d33ebd1540ff29e5e847dee781ddd9740a2bcc27
kySuga <kyle@masuga.com>
2024-03-19 23:09:46 -0400

    Moved styles for header and footer into global, removed from modules and deleted headerFooter module. Added styles to global for form and other global elements.

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import styles from "./headerFooter.module.css";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footer__wrap}>
-        <nav className={styles.footer__nav}>
-          <ul className={styles.nav__ul}>
+    <footer className="footer">
+      <div className="footer__wrap">
+        <nav className="footer__nav">
+          <ul className="nav__ul">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
             <li><Link href="/my-work">My Work</Link></li>
@@ -14,12 +13,12 @@ export default function Footer() {
             <li><Link href="/blog">Blog</Link></li>
           </ul>
         </nav>
-        <div className={styles.footer__content}>
+        <div className="footer__content">
           <h2>
             Jessica Masuga Consulting, LLC
           </h2>
-          <p className={styles.copyright}>
-            &copy; <time id={styles.footer__date}>1973</time> — Jessica Masuga Consluting, LLC
+          <p className="copyright">
+            &copy; <time id="footer__date">1973</time> — Jessica Masuga Consluting, LLC
           </p>
         </div>
       </div>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,15 +1,14 @@
 import Link from "next/link";
-import styles from "./headerFooter.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.header__inner}>
-        <div className={styles.logo__wrapper}>
-          <a className={styles.logo} href="/">Jessica Masuga Consulting</a>
+    <header className="header">
+      <div className="header__inner">
+        <div className="logo__wrapper">
+          <a className="logo" href="/">Jessica Masuga Consulting</a>
         </div>
-        <nav className={styles.nav}>
-          <ul className={styles.nav__ul}>
+        <nav className="nav">
+          <ul className="nav__ul">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
             <li><Link href="/my-work">My Work</Link></li>
