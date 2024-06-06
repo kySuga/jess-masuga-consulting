ddc8ab417a513cdece26afc11a123514bf48458a
kySuga <kyle@masuga.com>
2024-06-06 12:36:18 -0400

    Create contact form component, add to contact page. Adjust styles for textarea and global. Setup db.json file.

---
+++ src/app/components/contactForm.tsx
@@ -0,0 +1,26 @@
+// "use client"
+
+export default function contactForm() {
+  return (
+    <div className="form__wrap">
+      <form id="contact-form" action="">
+        <div className="form__element">
+          <label htmlFor="name">Name <span className="size-adj">(required)</span></label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="form__element">
+          <label htmlFor="email">Email <span className="size-adj">(required)</span></label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form__element">
+          <label htmlFor="message">Message <span className="size-adj">(required)</span></label>
+          <textarea id="message" name="message"></textarea>
+        </div>
+      </form>
+      <p className="size-adj">
+        This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.
+      </p>
+      <button type="submit" form="contact-form" value="Send">Send</button>
+    </div>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,5 @@
-"use client"
-
-import styles from "./contact.module.css"
+// components
+import ContactForm from "../components/contactForm";
 
 // contact page
 export default function Contact() {
@@ -14,26 +13,8 @@ export default function Contact() {
           Use the form below or email me at jessmasuga@hotmail.com
         </p>
       </div>
-      <div className="form__wrap">
-        <form id="contact-form" action="">
-          <div className="form__element">
-            <label htmlFor="name">Name <span className="size-adj">(required)</span></label>
-            <input type="text" id="name" name="name" required />
-          </div>
-          <div className="form__element">
-            <label htmlFor="email">Email <span className="size-adj">(required)</span></label>
-            <input type="email" id="email" name="email" required />
-          </div>
-          <div className="form__element">
-            <label htmlFor="message">Message <span className="size-adj">(required)</span></label>
-            <textarea id="message" name="message"></textarea>
-          </div>
-        </form>
-        <p className="size-adj">
-          This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.
-        </p>
-        <button type="submit" form="contact-form" value="Send">Send</button>
-      </div>
+
+      <ContactForm />
     </>
   );
 }
