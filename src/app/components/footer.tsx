782bfcc2442562d2a4f8e81f3fc122560b332817
kySuga <kyle@masuga.com>
2024-06-27 20:49:36 -0400

    Add JS for copyright date. Adjust header and footer styles.

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,24 +1,40 @@
 import Link from "next/link";
 
 export default function Footer() {
+  // set date in footer
+  const footerDate = new Date();
+  const currentYear = footerDate.getFullYear().toString();
+
   return (
     <footer className="footer">
       <div className="footer__wrap">
         <nav className="footer__nav">
           <ul className="nav__ul">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/my-work">My Work</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/my-work">My Work</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link href="/blog">Blog</Link>
+            </li>
           </ul>
         </nav>
         <div className="footer__content">
-          <h2>
-            Jessica Masuga Consulting, LLC
-          </h2>
+          <h2>Jessica Masuga Consulting, LLC</h2>
           <p className="copyright">
-            &copy; <time id="footer__date">1973</time> — Jessica Masuga Consluting, LLC
+            &copy;{" "}
+            <time id="footer__date" dateTime={currentYear}>
+              {currentYear}
+            </time>{" "}
+            — Jessica Masuga Consluting, LLC
           </p>
         </div>
       </div>
