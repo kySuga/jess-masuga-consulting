548e83ed78706f93f65ecc5c236683ba1bee1d30
kySuga <kyle@masuga.com>
2024-07-25 00:27:18 -0400

    Update styles and email on contact page.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,20 +1,30 @@
 // components
 import ContactForm from "../components/contactForm";
+import styles from "./contact.module.css";
 
 // contact page
 export default function Contact() {
+  const email = process.env.NEXT_PUBLIC_EMAIL;
+
   return (
     <>
-      <div className="banner secondary">
-        <h1>
-          Contact Me
-        </h1>
-        <p>
-          Use the form below or email me at jessmasuga@hotmail.com
-        </p>
+      <div className="top-to-bottom__wrap banner secondary">
+        <div className="gutter-alley__wrap">
+          <h1>Contact Me</h1>
+          <p>
+            Use the form below or email me at{" "}
+            <a className="hover__noLine" href={`mailto:${email}`}>
+              {email}
+            </a>
+          </p>
+        </div>
       </div>
 
-      <ContactForm />
+      <div className="top-to-bottom__wrap">
+        <div className="gutter-alley__wrap">
+          <ContactForm />
+        </div>
+      </div>
     </>
   );
 }
