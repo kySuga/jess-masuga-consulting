ad7d08f7819ea9981fc581a8b4685431ad4ee8c1
kySuga <kyle@masuga.com>
2024-07-24 23:59:19 -0400

    Tighten up styles for home and about pages, adding gutter-alley__wrap and top-to-bottom__wrap classes.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,76 +1,76 @@
-// import Image from "next/image";
+import Image from "next/image";
 import styles from "./about.module.css";
 
 // about page
 export default function Page() {
   return (
     <>
-      <div className={styles.about}>
-        <picture>
-          <source
-            srcSet="https://res.cloudinary.com/do9qt3sjt/image/upload/v1710699832/jmc-background-m_ixd7r6.jpg"
-            media="(max-width: 640px)"
-          />
-          <img
-            className={styles.about__img}
-            src="https://res.cloudinary.com/do9qt3sjt/image/upload/v1710727878/jmc-about_atbn2j.jpg"
-            alt=""
-          />
-        </picture>
-        <div className={styles.about__content}>
-          <h1>About Me</h1>
-          <p>
-            Need a savvy communications consultant? You’ve come to the right
-            place!
-          </p>
-          <h2>Below is a list of services I provide:</h2>
-          <ul>
-            <li>
-              <h3>Integrated Communications Planning</h3>
-              <p>
-                Create, refine, and execute integrated communications programs
-                to create meaningful connections to your brand.
-              </p>
-            </li>
-            <li>
-              <h3>Writing and Storytelling</h3>
-              <p>
-                Develop a breadth of content such as patient stories, listicles,
-                press releases, fact sheets, FAQs, and more.
-              </p>
-            </li>
-            <li>
-              <h3>Crisis Communications</h3>
-              <p>
-                Help your organization navigate a variety of issues, from
-                product recalls to corporate reputation matters.
-              </p>
-            </li>
-            <li>
-              <h3>Messaging and Narrative Development</h3>
-              <p>
-                Develop and refine your organization’s internal and external
-                messages to create a story to help you reach your target
-                audiences.
-              </p>
-            </li>
-            <li>
-              <h3>Media Strategy and Outreach</h3>
-              <p>
-                Create and implement earned media programs and pitches for
-                national, local, and trade media coverage.
-              </p>
-            </li>
-          </ul>
-          <p>
-            Please feel free to contact me via this form or at
-            jessmasuga@hotmail.com to discuss opportunities to work together.
-          </p>
+      <div className="top-to-bottom__wrap">
+        <div className={`gutter-alley__wrap ${styles.about}`}>
+          <div className={styles.about__intro}>
+            <h1>My Background & Services I Offer</h1>
+            <Image
+              className={styles.about__img}
+              src="https://res.cloudinary.com/do9qt3sjt/image/upload/v1710727878/jmc-about_atbn2j.jpg"
+              width={680}
+              height={416}
+              alt="Typewriter that reads, Stories matter."
+            />
+          </div>
+          <div className={styles.about__content}>
+            <p>
+              Need a savvy communications consultant? You’ve come to the right
+              place!
+            </p>
+            <h2>Below is a list of services I provide:</h2>
+            <ul>
+              <li>
+                <h3>Integrated Communications Planning</h3>
+                <p>
+                  Create, refine, and execute integrated communications programs
+                  to create meaningful connections to your brand.
+                </p>
+              </li>
+              <li>
+                <h3>Writing and Storytelling</h3>
+                <p>
+                  Develop a breadth of content such as patient stories,
+                  listicles, press releases, fact sheets, FAQs, and more.
+                </p>
+              </li>
+              <li>
+                <h3>Crisis Communications</h3>
+                <p>
+                  Help your organization navigate a variety of issues, from
+                  product recalls to corporate reputation matters.
+                </p>
+              </li>
+              <li>
+                <h3>Messaging and Narrative Development</h3>
+                <p>
+                  Develop and refine your organization’s internal and external
+                  messages to create a story to help you reach your target
+                  audiences.
+                </p>
+              </li>
+              <li>
+                <h3>Media Strategy and Outreach</h3>
+                <p>
+                  Create and implement earned media programs and pitches for
+                  national, local, and trade media coverage.
+                </p>
+              </li>
+            </ul>
+            <p>
+              Please feel free to contact me via this form or at
+              jessmasuga@hotmail.com to discuss opportunities to work together.
+            </p>
+          </div>
         </div>
       </div>
 
-      <div className="banner wBorderBottom">
-        <div className={styles.content__quote}>
+      <div className="top-to-bottom__wrap banner wBorderBottom">
+        <div className={`gutter-alley__wrap ${styles.content__quote}`}>
           <figure>
             <blockquote>
               “Good communication is the bridge between confusion and clarity.”
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,36 +8,38 @@ export default function Page() {
   return (
     <>
       <div className={styles.hero}>
-        <div className={styles.hero__content}>
-          <div className={styles.hero__heading}>
-            <Image
-              className={styles.hero__profileImg}
-              src="https://res.cloudinary.com/do9qt3sjt/image/upload/v1710642006/jessica-masuga_ktzf5n.jpg"
-              loading="eager"
-              width={300}
-              height={300}
-              alt="Jessica Masuga."
-            />
-            <h1>Jessica Masuga Consulting</h1>
+        <div className={`gutter-alley__wrap ${styles.hero__content}`}>
+          <div className="top-to-bottom__wrap">
+            <div className={styles.hero__heading}>
+              <Image
+                className={styles.hero__profileImg}
+                src="https://res.cloudinary.com/do9qt3sjt/image/upload/v1710642006/jessica-masuga_ktzf5n.jpg"
+                loading="eager"
+                width={300}
+                height={300}
+                alt="Jessica Masuga."
+              />
+              <h1>Jessica Masuga Consulting</h1>
+            </div>
+            <p>
+              I’m an expert storyteller who creates a broad range of impactful,
+              high-quality healthcare communications programs. My focus is on
+              building emotional connections between my clients and their key
+              audiences because great communications efforts are rooted in the
+              stories we tell.
+            </p>
+            <p>
+              I have more than 20 years of experience leading corporate and
+              product communication programs; 15 years of corporate healthcare
+              communications experience and 9 years of PR agency experience.
+            </p>
+            <p>
+              I specialize in integrated communications for healthcare companies
+              including biotech, medtech, medical device, pharma, and
+              nonprofits. I’m ready to support any size program in partnership
+              with your organization.
+            </p>
           </div>
-          <p>
-            I’m an expert storyteller who creates a broad range of impactful,
-            high-quality healthcare communications programs. My focus is on
-            building emotional connections between my clients and their key
-            audiences because great communications efforts are rooted in the
-            stories we tell.
-          </p>
-          <p>
-            I have more than 20 years of experience leading corporate and
-            product communication programs; 15 years of corporate healthcare
-            communications experience and 9 years of PR agency experience.
-          </p>
-          <p>
-            I specialize in integrated communications for healthcare companies
-            including biotech, medtech, medical device, pharma, and nonprofits.
-            I’m ready to support any size program in partnership with your
-            organization.
-          </p>
         </div>
         <picture>
           <source
@@ -63,8 +65,8 @@ export default function Page() {
         </picture>
       </div>
 
-      <div className="banner">
-        <div className={styles.content__contact}>
+      <div className="top-to-bottom__wrap banner">
+        <div className={`gutter-alley__wrap ${styles.content__contact}`}>
           <h2>Contact Me</h2>
           <p>
             My inbox is always open, you can contact me via{" "}
@@ -80,17 +82,21 @@ export default function Page() {
         </div>
       </div>
 
-      <div className={`${styles.wrap} ${styles.campaign}`}>
-        <div className={styles.content__campaign}>
-          <h2>Campaigns & Efforts</h2>
-          <p>Explore a featured selection of my campaigns and efforts below.</p>
+      <div className="gutter-alley__wrap">
+        <div className={`top-to-bottom__wrap ${styles.campaign}`}>
+          <div className={styles.content__campaign}>
+            <h2>Campaigns & Efforts</h2>
+            <p>
+              Explore a featured selection of my campaigns and efforts below.
+            </p>
+          </div>
+          <Image
+            src="https://res.cloudinary.com/do9qt3sjt/image/upload/v1710643849/campaigns-and-efforts-placeholder_hucrcr.png"
+            width={300}
+            height={300}
+            alt=""
+          />
         </div>
-        <Image
-          src="https://res.cloudinary.com/do9qt3sjt/image/upload/v1710643849/campaigns-and-efforts-placeholder_hucrcr.png"
-          width={300}
-          height={300}
-          alt=""
-        />
       </div>
     </>
   );
